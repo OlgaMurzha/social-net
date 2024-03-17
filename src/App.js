dc506eb127309ba1475b669dc5e63a2c6ac27a54
OlgaMurzha <olga.murzha27@gmail.com>
2024-03-17 19:13:30 +0100

    Components added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <div className="app-wrapper">
+            <header className="header">
+                <img src="https://img.freepik.com/free-vector/bird-colorful-gradient-design-vector_343694-2506.jpg" alt=""></img>
+            </header>
+            <nav className="nav">
+                <div className="content">
+                    <div>
+                        Profile
+                    </div>
+                    <div>
+                        Messages
+                    </div>
+                    <div>
+                        Main Content
+                    </div>
+                </div>
+            </nav>
+        </div>
+    )
 }
 
 export default App;
